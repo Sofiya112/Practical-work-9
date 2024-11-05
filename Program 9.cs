fa750c3b44f5c566f4c8bcbdcf7d3f124b0d76b9
Sofiya112 <sofiya032005@gmail.com>
2024-11-05 14:33:39 +0200

    Add files via upload

---
+++ Program 9.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1.Определить, имеются ли в одномерном массиве случайных чисел элементы, значения которых кратны 11.Вывести такие значения.
+            //int[] omas = new int[10];
+            //for (int i = 0; i< 10; i++) ;
+            //{
+            //    Console.Write("Введите{i}элемент-  ");
+            //    omas[i] = int.Parse(Console.ReadLine());
+
+            //}
+            //foreach (int i in omas) ;
+            //{
+            //    if (i % 11 == 0) ;
+            //}
+            //Console.WriteLine(i);
+            //Console.Read();
+
+            //Задача 2.Найти сумму и количество положительных элементов в одномерном массиве с явной инициализацией.
+
+
+            //    int[] numbers = { 1, 2, 3, 4, 5 };
+            //    int sum = 0;
+            //    int count = numbers.Length;
+
+            //    foreach (int number in numbers)
+            //    {
+            //        sum += number;
+            //    }
+            //    Console.WriteLine("Сумма элементов массива: " + sum);
+            //Console.WriteLine("Количество элементов массива: " + count);
+            //Console.Read();
+
+            //Задача 3.Вывести элементы одномерного массива, заполненного с клавиатуры ,в обратном порядке.
+
+            //    Console.Write("Введите количество элементов массива: ");
+            //    int n = int.Parse(Console.ReadLine());
+            //    int[] array = new int[n];
+            //    for (int i = 0; i < n; i++)
+            //    {
+            //        Console.Write("Введите элемент " + (i + 1) + ": ");
+            //        array[i] = int.Parse(Console.ReadLine());
+            //    }
+            //    Console.WriteLine("Элементы массива в обратном порядке:");
+            //    for (int i = n - 1; i >= 0; i--)
+            //    {
+            //        Console.Write(array[i] + " ");
+            //    }
+            //Console.Read();
+        }
+        }
+    }
